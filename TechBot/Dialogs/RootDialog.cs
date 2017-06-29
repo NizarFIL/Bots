c877304832da85afe6c5a8ee0132d02428cde914
Ghribi, Nizar <Nizar.Ghribi@fil.com>
2017-06-29 09:41:43 +0100

    .

--- TechBot/Dialogs/RootDialog.cs
+++ TechBot/Dialogs/RootDialog.cs
@@ -2,62 +2,30 @@
 using System.Threading.Tasks;
 using Microsoft.Bot.Builder.Dialogs;
 using Microsoft.Bot.Connector;
-using Microsoft.Bot.Builder.Luis;
-using Microsoft.Bot.Builder.Luis.Models;
-using System.Collections.Generic;
 
 namespace TechBot.Dialogs
 {
-    //[Serializable]
-    //public class RootDialog : IDialog<object>
-    //{
-    //    public async Task StartAsync(IDialogContext context)
-    //    {
-    //        await context.PostAsync("Hello !");
-    //        context.Wait(MessageReceivedAsync);
-
-    //        return;
-    //    }
-
-    //    private async Task MessageReceivedAsync(IDialogContext context, IAwaitable<object> result)
-    //    {
-
-    //        await context.PostAsync("Hello !!!");
-
-    //        context.Wait(MessageReceivedAsync);
-    //    }
-    //}
-
-    [LuisModel("e7b5f21e-222a-41e3-aee9-07909a845e6a", "54b3a614d45047a5a53a46fe644be1b3")]
     [Serializable]
-    public class RootDialog : LuisDialog<object>
+    public class RootDialog : IDialog<object>
     {
-
-        [LuisIntent("None")]
-        [LuisIntent("")]
-        public async Task None(IDialogContext context, LuisResult result)
+        public Task StartAsync(IDialogContext context)
         {
-            await context.PostAsync("I'm sorry. I didn't understand you.");
-            context.Wait(MessageReceived);
+            context.Wait(MessageReceivedAsync);
+
+            return Task.CompletedTask;
         }
 
-        [LuisIntent("IncreaseQuota")]
-        public async Task IncreaseQuota(IDialogContext context, IAwaitable<IMessageActivity> activity, LuisResult result)
+        private async Task MessageReceivedAsync(IDialogContext context, IAwaitable<object> result)
         {
-            var message = await activity;
+            var activity = await result as Activity;
 
-            await context.PostAsync("You want to increase quota");
+            // calculate something for us to return
+            int length = (activity.Text ?? string.Empty).Length;
 
+            // return our reply to the user
+            await context.PostAsync($"You sent {activity.Text} which was {length} characters");
+
+            context.Wait(MessageReceivedAsync);
         }
-
-
-        [LuisIntent("Access.OnlineMeetingService")]
-        public async Task AccessOnlineMeetingService(IDialogContext context, IAwaitable<IMessageActivity> activity, LuisResult result)
-        {
-            var message = await activity;
-            await context.PostAsync("You want to access an online meeting");
-
-        }
-
     }
 }
